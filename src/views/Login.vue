eed2dadabe073824b2a99fa006e4728428dd99a4
a-pompom <aoi.matsuda.3x@gmail.com>
2020-01-18 13:56:36 +0900

    ログイン画面のスタイル修正
    ・クラス名が冗長になっている部分がいくつかあったので、Elementが多重階層とならないよう修正
    ・スタイルの構造が深くなり過ぎないよう意識し、極力構造とモジュール要素が重複しないようにした
    
    #2

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,40 @@
 <template>
-    <div>
-        Login
-    </div>
+	<div class="login-container">
+	
+		<header class="login-header">
+			
+			<h2>That Command</h2>
+			
+		</header>
+		
+		<div class="login-form">
+		
+			<form action="#" method="post">
+
+                <!-- ユーザ名 -->	
+				<label for="userName" class="login-input login-form__item">
+					<input type="text" placeholder=" " id="userName">
+					<span class="login-input__placeholder">Username</span>
+					<span class="login-input__border"></span>
+				</label>
+				
+                <!-- パスワード -->	
+				<label for="password" class="login-input login-form__item">
+					<input type="password" placeholder=" " id="password">
+					<span class="login-input__placeholder">Password</span>
+					<span class="login-input__border"></span>
+				</label>
+				
+				<br>
+
+				<input type="submit" 
+					value="LOGIN" class="login-button login-form__button">
+
+			</form>
+			
+		</div>
+		
+	</div>
 </template>
 
 <script>
